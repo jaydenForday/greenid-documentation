d9e894e6867a3a8b2253757f4f87ed031db06ec2
Jayden Forday <jayden.forday@gbgplc.com>
2024-11-07 16:26:12 +1100

    update tina/config.js

--- tina/config.js
+++ tina/config.js
@@ -1,18 +1,16 @@
 import { defineConfig } from "tinacms";
 
-// Your hosting provider likely exposes this as an environment variable
-const branch =
-  process.env.GITHUB_BRANCH ||
-  process.env.VERCEL_GIT_COMMIT_REF ||
-  process.env.HEAD ||
-  "main";
+// // Your hosting provider likely exposes this as an environment variable
+// const branch =
+//   process.env.GITHUB_BRANCH ||
+//   process.env.VERCEL_GIT_COMMIT_REF ||
+//   process.env.HEAD ||
+//   "main";
 
 export default defineConfig({
-  branch,
-
-  // Get this from tina.io
+  
+  branch: process.env.NEXT_PUBLIC_TINA_BRANCH || 'main',
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  // Get this from tina.io
   token: process.env.TINA_TOKEN,
 
   build: {
