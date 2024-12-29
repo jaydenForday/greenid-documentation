ef54db9a1844b16addc3e33e9db7cb1c735effd6
jaydenForday <88642456+jaydenForday@users.noreply.github.com>
2024-12-30 09:33:12 +1100

    Revert "fix up sidebar"

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,12 +40,6 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/jaydenForday/greenid-documentation',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
